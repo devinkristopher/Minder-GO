0d0f82e5ea41205813c7e5647bff10c65959fffa
devinkristopher <devinkristopher@icloud.com>
2023-12-30 20:59:40 -0500

    Initial Commit

---
+++ Fieldomation/FieldomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldomationApp.swift
+//  Fieldomation
+//
+//  Created by Devin Rogers on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FieldomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
