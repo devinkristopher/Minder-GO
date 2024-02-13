b2edc1986afaa318d02019220c594a42b029cfca
devinkristopher <devinkristopher@icloud.com>
2024-02-13 04:57:19 -0500

    adding ability to export CSV, cleaning up UI features

--- Fieldomation/ContentView.swift
+++ Fieldomation/ContentView.swift
@@ -8,18 +8,18 @@ import SafariServices
 import SwiftUI
 import VisionKit
 
+struct ScanData {
+    var asset: String
+}
+
 struct ContentView: View {
     @State var myUrl = ""
     @State var mySetString = ""
     @EnvironmentObject var vm: AppViewModel
+    @State var setRows = [String]()
     
     private let textContentTypes: [(title: String, textContentType: DataScannerViewController.TextContentType?)] = [
-        ("All", .none),
-        ("URL", .URL),
-        ("Phone", .telephoneNumber),
-        ("Email", .emailAddress),
-        ("Address", .fullStreetAddress),
-        
+        ("All", .none)
     ]
     
     var body: some View {
@@ -99,11 +99,6 @@ struct ContentView: View {
                 Toggle("Simultaneous object recognition", isOn: $vm.recognizesMultipleItems)
             }
             if vm.scanType == .format {
-                Picker("Text content type", selection: $vm.textContentType) {
-                    ForEach(textContentTypes, id: \.self.textContentType) { option in
-                        Text(option.title).tag(option.textContentType)
-                    }
-                }.pickerStyle(.segmented)
                 Toggle("Prepend Minder URL Path", isOn: $vm.prependsMinderURLPath)
             }
             if vm.scanType == .export {
@@ -117,24 +112,58 @@ struct ContentView: View {
                     Image(systemName: "list.bullet.rectangle.fill")
                         .font(.system(size: 60))
                 }
-                Button("Export as CSV", action: { readSetFromFile() })
-                Button {
-                } label: {
-                    Image(systemName: "printer.fill")
+                ShareLink(item:generateCSV()) {
+                    Label("Export as CSV", systemImage: "list.bullet.rectangle.portrait")
                         .font(.system(size: 60))
-
-                }
-                Button("Send as email", action: { readSetFromFile() })
-                Button {
-                } label: {
-                    Image(systemName: "envelope.fill")
-                        .font(.system(size: 60))
-
                 }
                 
             }
         }.padding(.horizontal)
     }
+    let scanData: [ScanData] = [
+            ScanData(asset: "Day 1"),
+            ScanData(asset: "Day 2"),
+            ScanData(asset: "Day 3"),
+            ScanData(asset: "Day 4")
+        ]
+
+    func generateCSV() -> URL {
+            var fileURL: URL!
+            
+        let time = NSDate().timeIntervalSince1970
+        let myTimeInterval = TimeInterval(time)
+        let heading = NSDate(timeIntervalSince1970: TimeInterval(myTimeInterval)).description
+        
+            // file rows
+        var rows = scanData.map { "\($0.asset)" }
+        
+        for i in 0..<setRows.count{
+            rows.append(setRows[i])
+        }
+                    
+            
+            // rows to string data
+            let stringData = heading + rows.joined(separator: "\n")
+            
+            do {
+                
+                let path = try FileManager.default.url(for: .documentDirectory,
+                                                       in: .allDomainsMask,
+                                                       appropriateFor: nil,
+                                                       create: false)
+                
+                fileURL = path.appendingPathComponent("Trip-Data.csv")
+                
+                // append string data to file
+                try stringData.write(to: fileURL, atomically: true , encoding: .utf8)
+                print(fileURL!)
+                
+            } catch {
+                print("error generating csv file")
+            }
+            return fileURL
+        }
+
     
     func readSetFromFile() {
         let file = "setdata.txt" //this is the file. we will write to and read from it
@@ -148,22 +177,24 @@ struct ContentView: View {
                 mySetString = text2
                 mySetString = String(mySetString.dropFirst())
                 mySetString = String(mySetString.dropLast())
-                var rows = mySetString.components(separatedBy: ", ")
+                setRows = mySetString.components(separatedBy: ", ")
                 
 
-                for i in rows.indices {
-                    rows[i] = rows[i].replacingOccurrences(of: "\"", with: "")
-                    if (vm.prependsMinderURLPath == true) { rows[i] = "https://v2.minder.io/miner/" + rows[i] }
+                for i in setRows.indices {
+                    setRows[i] = setRows[i].replacingOccurrences(of: "\"", with: "")
+                    if (vm.prependsMinderURLPath == true) { setRows[i] = "https://v2.minder.io/miner/" + setRows[i] }
                 }
                 
                 
-                print(rows)
-                mySetString = rows.description
+                print(setRows)
+                mySetString = setRows.description
             }
             catch {/* error handling here */}
             
         }
     }
+    
+    
     
     @ViewBuilder
     private var bottomContainerView: some View {
@@ -174,7 +205,9 @@ struct ContentView: View {
                     ForEach(vm.recognizedItems) { item in
                         switch item {
                         case .barcode(let barcode):
-                            Text("https://v2.minder.io/miner/\(barcode.payloadStringValue ?? "Unknown barcode")")
+                            // Commented line below shows clickable URL to miner
+                            // Text("https://v2.minder.io/miner/\(barcode.payloadStringValue ?? "Unknown barcode")")
+                            Text("\(barcode.payloadStringValue ?? "Unknown barcode")")
                         case .text(let text):
                             Text(text.transcript)
                             
@@ -190,6 +223,7 @@ struct ContentView: View {
         }
     }
     
-    
-  
+
 }
+    
+
